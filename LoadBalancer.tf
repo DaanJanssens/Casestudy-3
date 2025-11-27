72ad7a191f3709d2aafffe96f2e475b9de92097f
DaanJanssens <555086@student.fontys.nl>
2025-11-27 12:18:11 +0100

    Changes for the listener to work

--- LoadBalancer.tf
+++ LoadBalancer.tf
@@ -11,6 +11,7 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.innovatech.id
+  target_type = "ip"
 
   health_check {
     path = "/"
--- SecurityGroups.tf
+++ SecurityGroups.tf
@@ -58,13 +58,6 @@ resource "aws_security_group" "web_sg" {
     security_groups = [aws_security_group.loadbalancer_sg.id]
   }
 
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.loadbalancer_sg.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
