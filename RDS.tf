ccd8e73f87ca7b2ec9e01716300e15c21510d8cb
DaanJanssens <555086@student.fontys.nl>
2025-12-03 10:24:28 +0100

    Namming change and removed the autoscaling

--- RDS.tf
+++ RDS.tf
@@ -8,22 +8,20 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 }
 
 resource "aws_db_instance" "mysql_db" {
-  identifier = "innovatech-hrapp-mysql"
+  identifier = "innovatech_hrapp_mysql"
   engine = "mysql"
   engine_version = "8.0"
   instance_class = "db.t3.micro"
   allocated_storage = 20
-  max_allocated_storage = 100
-  db_name = "innovatech-hr"
+  db_name = "innovatech_hr"
   username = var.db_user
   password = var.db_password
   publicly_accessible = false
-  storage_type = "gp3"
-  vpc_security_group_ids = [aws_security_group.database_sg.id]
+  vpc_security_group_ids = aws_security_group.database_sg.id
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
   skip_final_snapshot = true
 
   tags = {
-    Name = "innovatech-hrapp-mysql-db"
+    Name = "innovatech_hrapp_mysql_db"
   }
 }
