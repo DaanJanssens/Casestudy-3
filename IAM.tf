9395c18d1bcaee090a6d5c704646202820a0df76
DaanJanssens <daanjanssens6@gmail.com>
2025-12-11 14:55:40 +0100

    Change in Statement

--- IAM.tf
+++ IAM.tf
@@ -87,21 +87,23 @@ resource "aws_iam_role_policy" "lambda_auto_ops_policy" {
   role = aws_iam_role.lambda_auto_ops_role.id
 
   policy = jsonencode({
-    Version ="2012-20-17"
-    Statement =[{
-      Effect = "Allow"
-      Action = [
-        "ecs:UpdateService",
-        "ecs:DescribeServices",
-        "ecs:DescribeClusters",
-        "rds:StartDBInstance",
-        "rds:StopDBInstance",
-        "rds:DescribeDBInstances",
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource ="*"
-    }]
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices",
+          "ecs:DescribeClusters",
+          "rds:StartDBInstance",
+          "rds:StopDBInstance",
+          "rds:DescribeDBInstances",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
   })
 }
