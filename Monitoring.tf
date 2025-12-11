8be0faf9b58459ca1670d62c7fbcb121c1c85f19
DaanJanssens <daanjanssens6@gmail.com>
2025-12-11 12:00:44 +0100

    Monitoring dashboards

---
+++ Monitoring.tf
@@ -0,0 +1,208 @@
+resource "aws_cloudwatch_dashboard" "fargate_dashboard" {
+  dashboard_name = "fargate-fargate-instances"
+
+  dashboard_body = jsonencode({
+    widgets = [
+
+      {
+        "type" : "metric",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Fargate CPU Utilization",
+          "view" : "timeSeries",
+          "region" : "eu-central-1",
+          "metrics" : [
+            [
+              "AWS/ECS",
+              "CPUUtilization",
+              "ClusterName",
+              "faregate-cluster",
+              "ServiceName",
+              "innovatech_web_service"
+            ]
+          ],
+          "stat" : "Average",
+          "period" : 60
+        }
+      },
+
+      {
+        "type" : "metric",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Fargate Memory Utilization",
+          "view" : "timeSeries",
+          "region" : "eu-central-1",
+          "metrics" : [
+            [
+              "AWS/ECS",
+              "MemoryUtilization",
+              "ClusterName",
+              "faregate-cluster",
+              "ServiceName",
+              "innovatech_web_service"
+            ]
+          ],
+          "stat" : "Average",
+          "period" : 60
+        }
+      },
+
+      {
+        "type" : "metric",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Fargate Network (Rx / Tx)",
+          "view" : "timeSeries",
+          "region" : "eu-central-1",
+          "metrics" : [
+            [
+              "AWS/ECS",
+              "NetworkRxBytes",
+              "ClusterName",
+              "faregate-cluster",
+              "ServiceName",
+              "innovatech_web_service"
+            ],
+            [
+              "AWS/ECS",
+              "NetworkTxBytes",
+              "ClusterName",
+              "faregate-cluster",
+              "ServiceName",
+              "innovatech_web_service"
+            ]
+          ],
+          "stat" : "Sum",
+          "period" : 60
+        }
+      },
+
+      {
+        "type" : "metric",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Fargate Running Task Count",
+          "view" : "timeSeries",
+          "region" : "eu-central-1",
+          "metrics" : [
+            [
+              "AWS/ECS",
+              "RunningTaskCount",
+              "ClusterName",
+              "faregate-cluster",
+              "ServiceName",
+              "innovatech_web_service"
+            ]
+          ],
+          "stat" : "Average",
+          "period" : 60
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_dashboard" "rds_dashboard" {
+  dashboard_name = "rds-hrappdb-dashboard"
+
+  dashboard_body = jsonencode({
+    widgets = [
+
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 0,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "RDS CPU Utilization",
+          "metrics" : [
+            ["AWS/RDS", "CPUUtilization", "DBInstanceIdentifier", "hrappdb"]
+          ],
+          "stat" : "Average",
+          "period" : 60,
+          "region" : "eu-central-1"
+        }
+      },
+
+
+      {
+        "type" : "metric",
+        "x" : 12,
+        "y" : 0,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Free Storage Space",
+          "metrics" : [
+            ["AWS/RDS", "FreeStorageSpace", "DBInstanceIdentifier", "hrappdb"]
+          ],
+          "stat" : "Average",
+          "period" : 300,
+          "region" : "eu-central-1"
+        }
+      },
+
+
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 6,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Freeable Memory",
+          "metrics" : [
+            ["AWS/RDS", "FreeableMemory", "DBInstanceIdentifier", "hrappdb"]
+          ],
+          "stat" : "Average",
+          "period" : 300,
+          "region" : "eu-central-1"
+        }
+      },
+
+
+      {
+        "type" : "metric",
+        "x" : 12,
+        "y" : 6,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Database Connections",
+          "metrics" : [
+            ["AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", "hrappdb"]
+          ],
+          "stat" : "Average",
+          "period" : 60,
+          "region" : "eu-central-1"
+        }
+      },
+
+
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 18,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "Network Throughput (Receive / Transmit)",
+          "metrics" : [
+            ["AWS/RDS", "NetworkReceiveThroughput", "DBInstanceIdentifier", "hrappdb"],
+            ["AWS/RDS", "NetworkTransmitThroughput", "DBInstanceIdentifier", "hrappdb"]
+          ],
+          "stat" : "Average",
+          "period" : 60,
+          "region" : "eu-central-1"
+        }
+      }
+    ]
+  })
+}
+
